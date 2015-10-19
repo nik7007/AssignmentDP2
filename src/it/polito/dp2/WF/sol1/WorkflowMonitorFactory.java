bf83de9322a6528e9dcb4b6747af1d12e3a97927
nik7 <nik7007@users.noreply.github.com>
2015-10-19 22:22:12 +0200

    Start creating new stuff

---
+++ src/it/polito/dp2/WF/sol1/WorkflowMonitorFactory.java
@@ -0,0 +1,13 @@
+package it.polito.dp2.WF.sol1;
+
+
+import it.polito.dp2.WF.WorkflowMonitor;
+import it.polito.dp2.WF.WorkflowMonitorException;
+
+public class WorkflowMonitorFactory extends it.polito.dp2.WF.WorkflowMonitorFactory {
+
+    @Override
+    public WorkflowMonitor newWorkflowMonitor() throws WorkflowMonitorException {
+        return null;
+    }
+}
