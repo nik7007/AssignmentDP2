0884361b80a47e4b873700430699a3026e064c1b
nik7 <nik7007@users.noreply.github.com>
2015-10-24 01:29:22 +0200

    Implement new things

---
+++ src/it/polito/dp2/WF/sol1/lib/ProcessReaderImp.java
@@ -0,0 +1,28 @@
+package it.polito.dp2.WF.sol1.lib;
+
+import it.polito.dp2.WF.ActionStatusReader;
+import it.polito.dp2.WF.ProcessReader;
+import it.polito.dp2.WF.WorkflowReader;
+
+import java.util.Calendar;
+import java.util.List;
+
+public class ProcessReaderImp implements ProcessReader {
+
+
+
+    @Override
+    public Calendar getStartTime() {
+        return null;
+    }
+
+    @Override
+    public WorkflowReader getWorkflow() {
+        return null;
+    }
+
+    @Override
+    public List<ActionStatusReader> getStatus() {
+        return null;
+    }
+}
