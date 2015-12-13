1c3c3523c3eeab85c1c80fea80b17722eba4a9dc
Nik <niky006@gmail.com>
2015-12-13 12:52:51 +0100

    Part2 done!!

--- src/it/polito/dp2/WF/sol2/WorkflowMonitorFactory.java
+++ src/it/polito/dp2/WF/sol2/WorkflowMonitorFactory.java
@@ -4,10 +4,16 @@ package it.polito.dp2.WF.sol2;
 import it.polito.dp2.WF.WorkflowMonitor;
 import it.polito.dp2.WF.WorkflowMonitorException;
 import it.polito.dp2.WF.sol2.lib.WorkflowMonitorImpl;
+import it.polito.dp2.WF.sol2.lib.WorkflowReaderException;
 
 public class WorkflowMonitorFactory extends it.polito.dp2.WF.WorkflowMonitorFactory{
     @Override
     public WorkflowMonitor newWorkflowMonitor() throws WorkflowMonitorException {
-        return new WorkflowMonitorImpl();
+        try {
+            return new WorkflowMonitorImpl();
+        } catch (WorkflowReaderException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
--- src/it/polito/dp2/WF/sol2/lib/WorkflowMonitorImpl.java
+++ src/it/polito/dp2/WF/sol2/lib/WorkflowMonitorImpl.java
@@ -1,12 +1,8 @@
 package it.polito.dp2.WF.sol2.lib;
 
 
-import it.polito.dp2.WF.ProcessReader;
-import it.polito.dp2.WF.WorkflowMonitor;
-import it.polito.dp2.WF.WorkflowReader;
-import it.polito.dp2.WF.sol2.jaxb.ProcessType;
-import it.polito.dp2.WF.sol2.jaxb.RootType;
-import it.polito.dp2.WF.sol2.jaxb.WorkflowType;
+import it.polito.dp2.WF.*;
+import it.polito.dp2.WF.sol2.jaxb.*;
 import it.polito.dp2.WF.sol2.reference.Reference;
 import org.xml.sax.SAXException;
 
@@ -27,7 +23,7 @@ public class WorkflowMonitorImpl implements WorkflowMonitor {
     private Map<Calendar, ProcessReader> processReaderMap;
 
 
-    public WorkflowMonitorImpl() {
+    public WorkflowMonitorImpl() throws WorkflowReaderException {
         workflowReaderMap = new HashMap<>();
         processReaderMap = new HashMap<>();
         String fileName = System.getProperty("it.polito.dp2.WF.sol2.WorkflowInfo.file");
@@ -36,6 +32,136 @@ public class WorkflowMonitorImpl implements WorkflowMonitor {
         RootType root = createRoot(fileName);
         List<WorkflowType> workflowTypes = root.getWorkflow();
         List<ProcessType> processTypes = root.getProcess();
+
+        for (WorkflowType workflowType : workflowTypes) {
+
+            Map<String, List<String>> actionConnector = new HashMap<>();
+            Map<String, ActionReaderImp> allActions = new HashMap<>();
+
+            createWorkflow(workflowType, actionConnector, allActions);
+
+            for (Map.Entry<String, List<String>> entry : actionConnector.entrySet()) {
+                String actionKey = entry.getKey();
+                List<String> actionList = entry.getValue();
+
+                ActionReader actionReader = allActions.get(actionKey);
+
+                for (String action : actionList) {
+                    ActionReader actionR = allActions.get(action);
+                    if (actionR != null)
+                        ((SimpleActionReaderImp) actionReader).addActionReader(actionR);
+                }
+            }
+
+        }
+
+        for (ProcessType processType : processTypes) {
+
+            ProcessReader processReader = createProcess(processType);
+            processReaderMap.put(processReader.getStartTime(),processReader);
+
+        }
+    }
+
+    ProcessReader createProcess(ProcessType processType) throws WorkflowReaderException {
+
+        GregorianCalendar date = processType.getDate().toGregorianCalendar();
+        WorkflowReader workflowReader = getWorkflowReader(((WorkflowType) processType.getWorkflow()).getName());
+
+        ProcessReader processReader = new ProcessReaderImp(date, workflowReader);
+
+        List<ActionStatusType> actionStatusList = processType.getActionStatus();
+
+        for(ActionStatusType actionStatusType : actionStatusList){
+            ((ProcessReaderImp)processReader).addActionStatusReader(createActionStatus(actionStatusType));
+        }
+
+        return processReader;
+    }
+
+    ActionStatusReader createActionStatus(ActionStatusType actionStatusType) {
+
+        ActionStatusReader actionStatusReader = new ActionStatusReaderImp(actionStatusType.getName());
+
+        if (actionStatusType.getDate() != null)
+            ((ActionStatusReaderImp) actionStatusReader).terminate(actionStatusType.getDate().toGregorianCalendar());
+
+        if (actionStatusType.getActor() != null) {
+            ((ActionStatusReaderImp) actionStatusReader).takeInCharge(createActor(actionStatusType.getActor()));
+        }
+
+        return actionStatusReader;
+    }
+
+    Actor createActor(ActorType actorType) {
+        return new Actor(actorType.getName(), actorType.getRole());
+
+    }
+
+
+    WorkflowReader getWorkflowReader(String name) {
+
+        if (workflowReaderMap.containsKey(name))
+            return workflowReaderMap.get(name);
+        WorkflowReader workflowReader = new WorkflowReaderImp(name);
+
+        workflowReaderMap.put(name, workflowReader);
+        return workflowReader;
+    }
+
+    void createWorkflow(WorkflowType workflow, Map<String, List<String>> actionConnector, Map<String, ActionReaderImp> allActions) {
+
+        WorkflowReader workflowReader = createWorkflow(workflow);
+
+        List<ActionType> actionTypes = workflow.getSimpleActionOrProcessAction();
+
+        for (ActionType actionType : actionTypes) {
+            ActionReaderImp actionReaderImp = createAction(actionType, workflowReader, actionConnector);
+            allActions.put(actionReaderImp.getName(), actionReaderImp);
+        }
+
+    }
+
+    WorkflowReader createWorkflow(WorkflowType workflow) {
+        return getWorkflowReader(workflow.getName());
+
+    }
+
+    ActionReaderImp createAction(ActionType action, WorkflowReader workflowReader, Map<String, List<String>> actionConnector) {
+
+        ActionReaderImp actionReader;
+
+        String name = action.getName();
+        String role = action.getRole();
+        Boolean auto = action.isAuto();
+
+        if (action instanceof SimpleActionType) {
+
+            actionReader = new SimpleActionReaderImp(name, workflowReader, role, auto);
+
+            List<SubActionType> subActions = ((SimpleActionType) action).getSubAction();
+            if (subActions != null && !subActions.isEmpty()) {
+                List<String> subName = new LinkedList<>();
+                for (SubActionType subActionType : subActions) {
+                    subName.add(subActionType.getNameRef());
+                }
+                actionConnector.put(name, subName);
+
+            }
+
+        } else {
+
+            WorkflowType workflowType = (WorkflowType) ((ProcessActionType) action).getSubWorkflow();
+
+            WorkflowReader wr = createWorkflow(workflowType);
+
+            actionReader = new ProcessActionReaderImp(name, workflowReader, role, auto, wr);
+
+
+        }
+
+        return actionReader;
+
     }
 
 
