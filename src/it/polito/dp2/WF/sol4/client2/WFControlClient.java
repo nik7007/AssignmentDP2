f11f80178e8b42d7ace8426ca3697c06d81c8d40
nik7 <nik7007@users.noreply.github.com>
2016-01-15 11:03:59 +0100

    start working on client 2

---
+++ src/it/polito/dp2/WF/sol4/client2/WFControlClient.java
@@ -0,0 +1,14 @@
+package it.polito.dp2.WF.sol4.client2;
+
+
+public class WFControlClient {
+
+    public static void main(String[] args) {
+
+        if (args.length < 2) {
+            System.exit(2);
+        }
+
+    }
+
+}
