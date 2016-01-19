3936f42c546ca6224a95598bf8896b84f0e2ccde
nik7 <nik7007@users.noreply.github.com>
2016-01-19 12:20:40 +0100

    bug fix!!!

--- src/it/polito/dp2/WF/sol4/server/datamanager/ProcessHolder.java
+++ src/it/polito/dp2/WF/sol4/server/datamanager/ProcessHolder.java
@@ -5,14 +5,10 @@ import it.polito.dp2.WF.lab4.gen.ActionStatusType;
 import it.polito.dp2.WF.lab4.gen.ActorType;
 import it.polito.dp2.WF.lab4.gen.ProcessType;
 
-import java.util.GregorianCalendar;
-
 import javax.xml.datatype.DatatypeConfigurationException;
 import javax.xml.datatype.DatatypeFactory;
 import javax.xml.datatype.XMLGregorianCalendar;
-
-import com.sun.org.apache.regexp.internal.recompile;
-import com.sun.org.apache.xerces.internal.jaxp.datatype.XMLGregorianCalendarImpl;
+import java.util.GregorianCalendar;
 
 public class ProcessHolder {
 
@@ -29,12 +25,13 @@ public class ProcessHolder {
     }
 
     public ProcessType getProcess() {
-    	synchronized (this) {
-    		return (new ProcessCopier(process)).getProcess();
-		}
-    	
+        synchronized (this) {
+            ProcessType processType = (new ProcessCopier(process)).getProcess();
+            return processType;
+        }
+
     }
-    
+
     public ProcessType getProcessNoTSafe() {
         return process;
     }
@@ -43,87 +40,87 @@ public class ProcessHolder {
         lastMod = new GregorianCalendar();
         this.process = process;
     }
-    
-    private class ProcessCopier{
-    	private final ProcessType process;
-    	
-    	public ProcessCopier(ProcessType process){
-    		this.process = new ProcessType();
-    		this.process.setWorkflow(process.getWorkflow());
-    		XMLGregorianCalendar date;
-    		
-    		 try {
-				date = DatatypeFactory.newInstance().newXMLGregorianCalendar(process.getDate().toGregorianCalendar());
-			} catch (DatatypeConfigurationException e) {
-				//e.printStackTrace();
-				date = process.getDate();
-			}
-    		 
-    		 this.process.setDate(date);
-    		 
-    		 for(ActionStatusType action: process.getActionStatus()){
-    			 this.process.getActionStatus().add((new ActionStatusCopier(action)).getAction());
-    			 
-    		 }
-    	}
-    	
-    	
-    	public ProcessType getProcess(){
-    		
-    		return process;
-    	}
-    	
-    	
-    	private class ActionStatusCopier{
-    		private  ActionStatusType actionStatus;
-    		
-    		public ActionStatusCopier(ActionStatusType actionStatus){
-    			this.actionStatus = new ActionStatusType();
-    			XMLGregorianCalendar date;
-    			
-    			 try {
-    				 date = DatatypeFactory.newInstance().newXMLGregorianCalendar(actionStatus.getDate().toGregorianCalendar());
-    				 this.actionStatus.setDate(date);    				 
-				} catch (NullPointerException | DatatypeConfigurationException e) {
-					//e.printStackTrace();
-					date = actionStatus.getDate();
-				}
-    			 this.actionStatus.setDate(date);
-    			 this.actionStatus.setName(actionStatus.getName());
-    			 this.actionStatus.setTakenInCharge(actionStatus.isTakenInCharge());
-    			 this.actionStatus.setTerminated(actionStatus.isTerminated());
-    			 
-    			 this.actionStatus.setActor((new ActorCopier(actionStatus.getActor()).getActor()));
-    			 
-    			
-    		}
-    		
-    		public ActionStatusType getAction() {
-    				return actionStatus;
-			}
-    		
-    		private class ActorCopier{
-    			private ActorType actor;
-    			
-    			public ActorCopier(ActorType actor){
-    				if(actor!= null){
-    				this.actor = new ActorType();
-    				this.actor.setName(actor.getName());
-    				this.actor.setRole(actor.getRole());
-    				}
-    				this.actor = null;
-    			}
-    			
-    			public ActorType getActor() {
-    				return actor;
-					
-				}
-    			
-    		}
-    		
-    	}
-    	
-    	
+
+    private class ProcessCopier {
+        private final ProcessType process;
+
+        public ProcessCopier(ProcessType process) {
+            this.process = new ProcessType();
+            this.process.setWorkflow(process.getWorkflow());
+            XMLGregorianCalendar date;
+
+            try {
+                date = DatatypeFactory.newInstance().newXMLGregorianCalendar(process.getDate().toGregorianCalendar());
+            } catch (DatatypeConfigurationException e) {
+                //e.printStackTrace();
+                date = process.getDate();
+            }
+
+            this.process.setDate(date);
+
+            for (ActionStatusType action : process.getActionStatus()) {
+                this.process.getActionStatus().add((new ActionStatusCopier(action)).getAction());
+
+            }
+        }
+
+
+        public ProcessType getProcess() {
+
+            return process;
+        }
+
+
+        private class ActionStatusCopier {
+            private ActionStatusType actionStatus;
+
+            public ActionStatusCopier(ActionStatusType actionStatus) {
+                this.actionStatus = new ActionStatusType();
+                XMLGregorianCalendar date;
+
+                try {
+                    date = DatatypeFactory.newInstance().newXMLGregorianCalendar(actionStatus.getDate().toGregorianCalendar());
+                    this.actionStatus.setDate(date);
+                } catch (NullPointerException | DatatypeConfigurationException e) {
+                    //e.printStackTrace();
+                    date = actionStatus.getDate();
+                }
+                this.actionStatus.setDate(date);
+                this.actionStatus.setName(actionStatus.getName());
+                this.actionStatus.setTakenInCharge(actionStatus.isTakenInCharge());
+                this.actionStatus.setTerminated(actionStatus.isTerminated());
+
+                this.actionStatus.setActor((new ActorCopier(actionStatus.getActor()).getActor()));
+
+
+            }
+
+            public ActionStatusType getAction() {
+                return actionStatus;
+            }
+
+            private class ActorCopier {
+                private ActorType actor;
+
+                public ActorCopier(ActorType actor) {
+                    if (actor != null) {
+                        this.actor = new ActorType();
+                        this.actor.setName(actor.getName());
+                        this.actor.setRole(actor.getRole());
+                    } else
+                        this.actor = null;
+                }
+
+                public ActorType getActor() {
+                    return actor;
+
+                }
+
+            }
+
+        }
+
+
     }
 
 }
