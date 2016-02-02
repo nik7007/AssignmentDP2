217cf71f20e8c0fe32e89671ec0b97f3582f7acd
nik7 <nik7007@users.noreply.github.com>
2016-02-03 00:30:15 +0100

    Add more catch

--- src/it/polito/dp2/WF/sol4/server/WorkflowServer.java
+++ src/it/polito/dp2/WF/sol4/server/WorkflowServer.java
@@ -7,20 +7,30 @@ import it.polito.dp2.WF.sol4.server.reference.Reference;
 
 import javax.xml.ws.Endpoint;
 import java.util.concurrent.Executors;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class WorkflowServer {
 
     static final DataManager DATA_MANAGER = ElementConverter.init();
+    private static Logger logger = Logger.getLogger(WorkflowServer.class.getName());
 
     public static void main(String[] args) {
 
         Endpoint info = Endpoint.create(new WorkflowInfoService());
         info.setExecutor(Executors.newFixedThreadPool(Reference.THREAD_NUMBER));
-        info.publish("http://localhost:7071/wfinfo");
+
 
         Endpoint control = Endpoint.create(new WorkflowControlService());
         control.setExecutor(Executors.newFixedThreadPool(Reference.THREAD_NUMBER));
-        control.publish("http://localhost:7070/wfcontrol");
+
+        try {
+            info.publish("http://localhost:7071/wfinfo");
+            control.publish("http://localhost:7070/wfcontrol");
+
+        } catch (IllegalArgumentException | IllegalStateException e) {
+            logger.log(Level.SEVERE, "Error publishing services");
+        }
 
     }
 
